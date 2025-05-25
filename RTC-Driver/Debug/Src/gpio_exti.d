22f3c2d4527f458e7698f0c6abb7ec59ba999423
John Nario <johnnario@Johns-MacBook-Air.local>
2025-05-25 12:53:27 -0400

    Removed Header_files Project, added project template for RTC driver

---
+++ RTC-Driver/Debug/Src/gpio_exti.d
@@ -0,0 +1,16 @@
+Src/gpio_exti.o: ../Src/gpio_exti.c ../Inc/gpio_exti.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/core_cm4.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_version.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_compiler.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_gcc.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/mpu_armv7.h \
+ /Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/gpio_exti.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/core_cm4.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_version.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_compiler.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/cmsis_gcc.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Include/mpu_armv7.h:
+/Users/johnnario/STM32Projects/BaremetalProjects/Interrupts/chip-headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
